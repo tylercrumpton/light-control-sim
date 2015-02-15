5abce4680b9ad9ef1a28354560289754f43736ff
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-02-14 22:21:09 -0600

    Added framework for hardware REST API.

--- lightcontrol/server.py
+++ lightcontrol/server.py
@@ -12,6 +12,17 @@ class StaticHandler(tornado.web.StaticFileHandler):
             return super(StaticHandler, self).get_absolute_path(root, "index.html")
         else:
             return super(StaticHandler, self).get_absolute_path(root, path)
+            
+class SetLightByNameHandler(tornado.web.RequestHandler):
+    def get(self, name, command):
+        if command.lower() == "on":
+            response = "Turning %s light on." % name.lower()
+        if command.lower() == "off":
+            response = "Turning %s light off." % name.lower()
+        if command.lower() == "toggle":
+            response = "Toggling %s light." % name.lower()
+        print response
+        self.write(response)
         
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "lightsim")
@@ -19,6 +30,7 @@ settings = {
         
 application = tornado.web.Application([
     (r"/lightsim/(.*)", StaticHandler, dict(path=settings['static_path'])),
+    (r"/hardware/setlightbyname/([A-z]+)/([A-z]+)", SetLightByNameHandler),
     (r"/", MainHandler),
 ])
 
