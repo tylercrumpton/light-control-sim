a58a4010f043dbecac539ae678b6997ee5672c33
Tyler Crumpton <tyler.crumpton@gmail.com>
2015-02-15 19:27:19 -0600

    Commands now change actual named pins.

--- lightcontrol/server.py
+++ lightcontrol/server.py
@@ -3,8 +3,18 @@ import tornado.web
 import os
 import RPIO
 
+# Define the room LED IO pins:
+rooms = {"living":{"pin":2, "on":True},   #J8 pin: 3
+         "kitchen":{"pin":3, "on":True},  #J8 pin: 5
+         "bed1":{"pin":4, "on":True},     #J8 pin: 7
+         "bed2":{"pin":17, "on":True},    #J8 pin: 11
+         "bed3":{"pin":18, "on":True},    #J8 pin: 12
+         "laundry":{"pin":27, "on":True}, #J8 pin: 13
+        }
+
 # Setup the output pins:
-RPIO.setup(22, RPIO.OUT)
+for room in rooms:
+    RPIO.setup(rooms[room]["pin"], RPIO.OUT)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -19,14 +29,26 @@ class StaticHandler(tornado.web.StaticFileHandler):
             
 class SetLightByNameHandler(tornado.web.RequestHandler):
     def get(self, name, command):
-        if command.lower() == "on":
-            response = "Turning %s light on." % name.lower()
-            RPIO.output(22, True)
-        if command.lower() == "off":
-            response = "Turning %s light off." % name.lower()
-            RPIO.output(22, False)
-        if command.lower() == "toggle":
-            response = "Toggling %s light." % name.lower()
+        name_lower = name.lower()
+        command_lower = command.lower()
+        if name_lower in rooms.keys():
+            if command_lower == "on":
+                response = "Turning '{}' light on.".format(name_lower)
+                rooms[name_lower]["on"] = True
+                RPIO.output(rooms[name_lower]["pin"], True)
+            elif command_lower == "off":
+                response = "Turning '{}' light off.".format(name_lower)
+                rooms[name_lower]["on"] = True
+                RPIO.output(rooms[name_lower]["pin"], False)
+            elif command_lower == "toggle":
+                new_state = not rooms[name_lower]["on"]
+                rooms[name_lower]["on"] = new_state
+                response = "Toggling '{}' light from {}.".format(name_lower, "off to on" if new_state else "on to off")
+                RPIO.output(rooms[name_lower]["pin"], new_state)
+            else:
+                response = "Cannot perform command '{}' on room '{}'.".format(command_lower, name_lower)
+        else:
+            response = "No room with name '{}' exists.".format(name_lower)
         print response
         self.write(response)
         
